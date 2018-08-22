16c015f9472af209fbd10f392ceb3df855d8065e
barakat <barakat.8163@gmail.com>
2018-08-22 17:03:43 +0300

    start 2

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule  } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/components.component';
 
@@ -10,7 +10,8 @@ import { ComponentsComponent } from './components/components.component';
     ComponentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
