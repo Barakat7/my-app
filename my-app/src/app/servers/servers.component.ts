071f9ee569009d546625168d51ae6027b17b3a02
barakat <barakat.8163@gmail.com>
2018-08-31 01:16:39 +0300

    commit a

--- my-app/src/app/servers/servers.component.ts
+++ my-app/src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 allowNewServer = false ;
 serverCreationStatus = ' no server was  created ' ;
-
+serverName = 'TestServer' ;
   constructor() {
     setTimeout(()=>{
     this.allowNewServer = true
@@ -16,11 +16,12 @@ serverCreationStatus = ' no server was  created ' ;
 
   ngOnInit() {
   }
+  onUpdateServerName(  event :Event ){
+    // console.log((<HTMLInputElement>event.target).value);
+    this.serverName  = (<HTMLInputElement>event.target).value ;
+  }
 onCreateServer(){
-  this.serverCreationStatus = 'server was  created '
+  this.serverCreationStatus = 'server was  created ' + this.serverName
 }
-onUpdateServername(  event :any ){
-  console.log(event);
-  
-}
+
 }
